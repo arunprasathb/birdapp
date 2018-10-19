735d02ad6c3ffb3b219a737be345d310df750b15
Murugan M <murugan.m@ideas2it.com>
2018-10-19 17:36:51 +0530

    Payment api to update next payment option added

--- app/Providers/BookServiceProvider.php
+++ app/Providers/BookServiceProvider.php
@@ -91,7 +91,7 @@ class BookServiceProvider extends BaseServiceProvider {
                 ->select('species.*')
                 ->where('books.id',$request->book_id)
                 ->get();
-            }else{;
+            }else{
                 $species_result = books::join('species', 'species.book_id', '=', 'books.id')
                 ->select('species.*')
                 ->where('books.id',$request->book_id)
@@ -116,18 +116,32 @@ class BookServiceProvider extends BaseServiceProvider {
      */
     public function payment($data){
          try {
-            // books::where('id',$request->bookId)
-            // $userbook = book_payment::where('bookId', $data['book_id'])->where('user_id', $data['user_id']);
-            // print_r($userbook);die;
+            $userbook = book_payment::where('bookId', $data['book_id'])->where('user_id', $data['user_id'])->get();
             $bookPayment = new book_payment();
             $bookPayment->bookId = $data['book_id'];
             $bookPayment->user_id = $data['user_id'];
             $bookPayment->payment_status = $data['payment_status'];
             $bookPayment->transaction_id = $data['transaction_id'];
-            $bookPayment->amount = $data['amount'];
-            $bookPayment->download_count = $data['download_count'];
-            // print_r($bookPayment);die;
-            $bookPayment->save();
+            if(count($userbook) > 0){
+                if($userbook[0]['download_count'] != 3){
+                    if($userbook[0]['amount'] != ""){
+                        $bookPayment->amount = $userbook[0]['amount']+$data['amount'];    
+                    }
+                    if($userbook[0]['download_count'] == 1){ 
+                        $bookPayment->download_count = 2;    
+                    }else if($userbook[0]['download_count'] == 2){
+                        $bookPayment->download_count = 3;    
+                    }
+                }else{
+                    $bookPayment->amount = $userbook[0]['amount'];
+                    $bookPayment->download_count = 3;
+                }
+                $bookPaymentupdate = book_payment::where('id',$userbook[0]['id'])->update(['download_count'=>$bookPayment->download_count, 'amount'=>$bookPayment->amount]);
+            }else{
+                $bookPayment->amount = $data['amount'];
+                $bookPayment->download_count = $data['download_count'];
+                $bookPayment->save();
+            }
             BookServiceProvider::$data['status'] = 200;
             BookServiceProvider::$data['data'] = $bookPayment;
             BookServiceProvider::$data['message'] = trans('messages.payment_success');
